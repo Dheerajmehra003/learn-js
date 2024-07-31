fa65f43d387b1c9e26a753fb80a175f6a8a993fe
Dheerajmehra003 <dheerajsinghmehra0405@gmail.com>
2024-07-31 05:34:55 -0700

    learn js

---
+++ index.js
@@ -0,0 +1,44 @@
+// let a,b,c
+// a=2
+// b=3
+// c=a+b
+// console.log("the value of c is",c)
+// let x=4
+// console.log("the value of x is 4")
+// {
+//     let x=5
+//     console.log("the value of x is",x)
+// }
+// let text1= "what a very"
+// text1+=" nice day"
+// console.log(text1)
+// let x=4
+// let y=5
+// let z=x+y
+// let a=x-y
+// // let b=x*y
+// let c=x/y
+// console.log("Addition",x+y)
+// console.log("subtraction",x-y)
+// console.log("multiplication",x*y)
+// console.log("divison",x/y)
+// console.log("the of operator is ",typeof 0)
+// const person = {
+//     firstname: "john",
+//     lastname: "doe",
+//     age: "50",
+//     eyecolor: "blue",
+// fullname: function () {
+//     return person.firstname+" "+person.lastname;
+// }
+// };
+// console.log("the full name of the person is ",person.fullname())
+// const array=Object.values(person);
+// console.log(array)
+// let mystring= JSON.stringify(person);
+// console.log(mystring);
+// let string="siafasrfwoerrfpokrpo sjdfoasir i"
+// let length = string.length;
+// console.log("the length of the given string is ",length)
+let text= "we are the so called \"vikings\"from the world";
+console.log(text)
