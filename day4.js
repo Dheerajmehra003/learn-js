edcfc0406a90a749b4a803384c69d0c260235d42
Dheerajmehra003 <dheerajsinghmehra0405@gmail.com>
2024-08-04 19:58:58 +0530

    learn questions

---
+++ day4.js
@@ -0,0 +1,31 @@
+// let a = (input_array) => {
+//     input_array = input_array.sort((a, b) => a - b);
+//     let duplicate_elements = []
+//     for (index in input_array) {
+//         if (input_array[index] ===
+//             input_array[index - 1]) {
+//             duplicate_elements.push(
+//                 input_array[index]);
+//         }
+//     }
+//     return [...new Set(duplicate_elements)];
+// }
+// let arr = [1, 1, 2, 2, 3, 3, 4, 5, 6, 1];
+// console.log(a(arr));
+
+// let a = {};
+// let b = { key: "b" };
+// let c = { key: "c" };
+
+// a[b] = 123;
+// a[c] = 456;
+
+// console.log(a[b]);
+// let a = [10,2,3,4,]
+// let b = a.toString()
+// console.log(b)
+
+// let a = "this is a sentence"
+// const array=a.split("")
+// console.log(array)
+
