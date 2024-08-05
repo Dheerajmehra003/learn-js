9461f0e421502a07bd873f6bb99a45563c4d551a
Dheerajmehra003 <dheerajsinghmehra0405@gmail.com>
2024-08-05 18:14:39 +0530

    learning dom

---
+++ day5.js
@@ -0,0 +1,62 @@
+// // console.log(document)
+// // let div = document.querySelector("div");
+// // console.dir(div);
+// let h2=document.querySelector("h2")
+// console.dir(h2.innerText)
+// h2.innerText = h2.innerText + "from apna college students"
+
+// let divs=document.querySelectorAll(".box");
+// divs[0].innerText="unique value 1"
+// divs[1].innerText="unique value 2"
+// divs[2].innerText="unique value 3"
+
+// let idx=1;
+// for(div of divs){
+//    div.innerText=`unique value of ${idx}`;
+//    idx++;
+// }
+// let div= document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+// let name = div.getAttribute("name");
+// console.log(name);
+
+// let para=document.querySelector("p");
+// console.log(para.setAttribute("class","newclass"));
+
+// let newbtn= document.createElement("button")
+// newbtn.innerText="clickme"
+
+// let div=document.querySelector("div")
+// div.append(newbtn);
+// div.prepend(newbtn);
+// div.after(newbtn);
+
+// let p=document.querySelector("p")
+// p.after(newbtn)
+
+// let newheading=document.createElement("h1")
+// newheading.innerHTML=" <i>this is a new heading</i>";
+
+// document.querySelector("body").prepend(newheading)
+//  newheading.remove();
+
+// let newbtn= document.createElement("button")
+//  newbtn.innerText="clickme"
+//  newbtn.style.color="white";
+// newbtn.style.backgroundColor="red"
+//  document.querySelector("body").prepend(newbtn)
+
+// let para=document.querySelector("p");
+// console.log(para)
+
+// let btn=document.querySelector("button")
+// btn.onclick = ()=>{
+//  console.log("button was clicked")
+//  let a=23
+//  a++
+//  console.log(a)
+// }
+
