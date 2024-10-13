2302aaa5c6b8a6b944efc8433273fbeca1ce0e9c
Dheerajmehra003 <dheerajsinghmehra0405@gmail.com>
2024-10-13 14:09:44 +0530

    practice

---
+++ advancedjavascript.js
@@ -0,0 +1,44 @@
+// document.getElementById('btn').addEventListener('click', function () {
+//     console.log('button clicked')
+// })
+
+// var a = 10;
+// let b = 10;
+
+// console.log(a)
+
+// setTimeout(() => {
+//     console.log('timeout called')
+// }, 5000);
+
+
+// console.log(b)
+ 
+// function outer() {
+//     let c = 10;
+//     function inner() {
+//         console.log("c :", c);
+//     }
+//     inner()
+// }
+
+// outer();
+// const radius = [2, 4, 5, 6]
+
+// const Area = function(radius) {
+//    return Math.PI * radius * radius
+// }
+
+// const Circumstance = function(radius) {
+//    return Math.PI * radius * radius
+// }
+
+// const calculate = function(radius,logic) {
+//     const output = []
+//     for(let i = 0; i < radius.length; i++) {
+//         output.push(logic(radius[i]))
+//     }
+//     return output;  
+// }
+
+// console.log(calculate(radius,Area))
