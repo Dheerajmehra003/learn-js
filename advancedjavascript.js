c3e347d6c6b00b129e9005fc3a650923ebff98ec
Dheerajmehra003 <dheerajsinghmehra0405@gmail.com>
2024-10-14 10:28:45 +0530

    practice

--- advancedjavascript.js
+++ advancedjavascript.js
@@ -139,8 +139,61 @@
 // console.log(age)
 // console.log(typeof(users))
 // console.log(typeof(sum))
-// console.log(findFirstname)
+// console.log(findFirstname
 
-                            //    ------Promises-----
+//    ------Promises---
 
-                            
+const cart = ['kurta', 'pajama', 'shirt']
+
+
+// const promise = getCartData(cart)
+// console.log(promise)
+
+// promise.then(function(orderId) {
+//     console.log(orderId)
+//     return orderId
+// })
+// .then(function(orderId){
+//    return proceedToPayment(orderId)
+// })
+// .then(function(paymentInfo){
+//     console.log(paymentInfo)
+// })
+
+// .catch(function(error) {
+//     console.log(error.message)
+// })
+
+getCartData(cart)
+.then((orderId) => console.log(orderId))
+.then((orderId) => proceedToPayment(orderId))
+.then((paymentInfo) => console.log(paymentInfo))
+.catch((error) => console.log(error.message))
+
+
+function getCartData(cart) {
+
+    const pr = new Promise(function(resolve, reject) {
+        if(valitdateCart(cart)) {
+            const err = new Error("there is an error")
+            reject(err)
+        }
+        const orderId = '12345'
+        if(orderId) {
+            resolve(orderId)
+        }
+    })
+    
+    return pr;
+}
+
+function valitdateCart(cart) {
+    return false
+}
+
+function proceedToPayment(orderId) {
+    return new Promise( function(reslove, reject) {
+        reslove('payment was successful')
+    })
+}
+
